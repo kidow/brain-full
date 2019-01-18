57c3771fa57b04f0865d6c39b164cbeb6bb11e22
kidow <wcgo2ling@hanmail.net>
2019-01-18 20:28:29 +0900

    server 연결

---
+++ server.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const express = require('express')
+const next = require('next')
+const { PORT, NODE_ENV } = process.env
+
+const port = parseInt(PORT, 10) || 3000
+const dev = NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  const server = express()
+
+  server.use(express.json())
+  server.use(express.urlencoded({ extended: false }))
+
+  server.use((err, req, res, next) => {
+    if (err) {
+      console.error(err)
+      res.status(err.status)
+    }
+    res.json({ success: false, message: err.message })
+  })
+
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.use(handle).listen(port, err => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
