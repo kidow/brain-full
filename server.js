289514f21a89f6a0644d97122b0eb3bce2db9297
kidow <wcgo2ling@hanmail.net>
2019-01-18 20:39:21 +0900

    module-alias set

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const handle = app.getRequestHandler()
 app.prepare().then(() => {
   const server = express()
 
+  require('./lib/moduleAlias')
+
   server.use(express.json())
   server.use(express.urlencoded({ extended: false }))
 
